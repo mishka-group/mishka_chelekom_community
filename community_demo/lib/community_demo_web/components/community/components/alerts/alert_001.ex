9bbaa3a1ffa6c1431ddb1691330780de39850ee7
Mona Aghili <28711325+MonaAghili@users.noreply.github.com>
2025-01-24 16:56:49 +0330

    add two alerts components

---
+++ community_demo/lib/community_demo_web/components/community/components/alerts/alert_001.ex
@@ -0,0 +1,26 @@
+defmodule CommunityDemoWeb.Community.Components.Alerts.Alert001 do
+  use Phoenix.Component
+  import CommunityDemoWeb.Components.{Alert}
+
+  @doc type: :component
+  attr(:id, :string,
+    default: nil,
+    doc: "A unique identifier is used to manage state and interaction"
+  )
+
+  attr(:class, :string, default: nil, doc: "")
+  attr(:rest, :global,
+    doc:
+      "Global attributes can define defaults which are merged with attributes provided by the caller"
+  )
+
+  slot(:inner_block, required: false, doc: "Inner block that renders HEEx content")
+
+  def community_alert_001(assigns) do
+    ~H"""
+    <.alert id={@id} variant="outline" kind={:warning} class={@class} icon={nil} rounded="large">
+      {render_slot(@inner_block)}
+    </.alert>
+    """
+  end
+end
