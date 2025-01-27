6562e8634072c18ffa2731edea2dacf0d73a982d
Mona Aghili <28711325+MonaAghili@users.noreply.github.com>
2025-01-27 13:46:19 +0330

    Add info header component

--- community_demo/lib/community_demo_web/live/home_live.ex
+++ community_demo/lib/community_demo_web/live/home_live.ex
@@ -1,5 +1,6 @@
 defmodule CommunityDemoWeb.HomeLive do
   use CommunityDemoWeb, :live_view
+  import CommunityDemoWeb.Components.{CustomFooter, CustomHeader}
 
   def mount(_params, _session, socket) do
     socket =
@@ -11,6 +12,13 @@ defmodule CommunityDemoWeb.HomeLive do
 
   def render(assigns) do
     ~H"""
+    <.custom_header>
+      <:link heading="Mishka Chelekom" description="This project has been developed using the components of Mishka Chelekom." title="Mishka Chelekom" url="https://github.com/mishka-group/mishka_chelekom" image="/images/mishka-logo.svg" />
+      <:link heading="Author" description="This Project was written by Mona Aghili." title="Mona Aghili Github" url="https://github.com/MonaAghili"  />
+      <:link heading="Docs" description="For detailed information about this component and its usage check out the docs." url="https://mishka.tools/chelekom/docs"  />
+      <:link heading="Source" description="The design and UI components of this project were inspired by a Figma template." title="Figma source" url="https://www.figma.com/design/aUDatlc87xWZr0JJzpKh3K/Tailwind-UI-Components-for-Figma-%7C-TailGrids-(Community)-(Copy)?t=a3pLFSaHtphs6ZIb-0"  />
+    </.custom_header>
+
     <section class="text-[#202020] dark:text-[#f6f6f6]">
       <div class={["max-w-6xl mx-auto px-5 lg:px-2 space-y-10 py-16"]}>
         <h4 class="text-center sm:text-2xl lg:text-4xl font-bold dark:text-[#cacfd2]">
@@ -3539,24 +3547,7 @@ defmodule CommunityDemoWeb.HomeLive do
         </div>
       </div>
     </section>
-    <footer class="footer-custom-component dark:text-[#f6f6f6] text-[13px] text-center">
-      <div class="max-w-5xl mx-auto px-5 py-4 space-y-2 border-t border-[#505050]">
-        <p>© 2024 Mishka. All rights reserved.</p>
-
-        <div class="text-[#535353] dark:text-[#cacfd2]">
-          <span class="cursor-pointer group">
-            <span>Based in Netherlands</span><svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              class="size-4 ms-1 -mt-0.5 inline-block group-hover:text-rose-600 transition-all duration-75"
-            >
-              <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
-            </svg>
-          </span>
-        </div>
-      </div>
-    </footer>
+    <.custom_footer />
     """
   end
 end
