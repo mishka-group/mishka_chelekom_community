7eab2db161f31a1817ecc590b1ae521a0a72a26b
Mona Aghili <28711325+MonaAghili@users.noreply.github.com>
2025-01-29 13:26:04 +0330

    Feat: add list components

---
+++ community_demo/lib/community_demo_web/components/community/components/lists/list_001.ex
@@ -0,0 +1,45 @@
+defmodule CommunityDemoWeb.Community.Components.Lists.List001 do
+  use Phoenix.Component
+  import CommunityDemoWeb.Components.{List}
+
+  @doc type: :component
+  attr(:id, :string,
+    default: nil,
+    doc: "A unique identifier is used to manage state and interaction"
+  )
+
+  attr(:class, :string,
+    default: nil,
+    doc: "custom classes"
+  )
+
+  attr(:rest, :global,
+    doc:
+      "Global attributes can define defaults which are merged with attributes provided by the caller"
+  )
+
+  slot :item, required: true do
+    attr(:class, :string, doc: "Custom CSS class for additional styling")
+  end
+
+  slot(:inner_block, required: false, doc: "Inner block that renders HEEx content")
+
+  def community_list_001(assigns) do
+    ~H"""
+    <.list
+      space="extra_small"
+      id={@id}
+      class={@class}
+      rounded="none"
+    >
+      <.li :for={{item, index} <- Enum.with_index(@item, 1)} padding="py-1 px-2" class={item[:class]}>
+        <div class="flex items-center gap-2">
+          <span class="bg-[#3758F9] text-white rounded-full size-5 flex leading-5 text-[13px] items-center justify-center">{index}</span>
+          {render_slot(item)}
+        </div>
+      </.li>
+    </.list>
+
+    """
+  end
+end
--- community_demo/lib/community_demo_web/live/lists_live.ex
+++ community_demo/lib/community_demo_web/live/lists_live.ex
@@ -1,5 +1,14 @@
 defmodule CommunityDemoWeb.Demo.Community.ListsLive do
   use CommunityDemoWeb, :live_view
+  import CommunityDemoWeb.Components.{CustomHeader, CustomCommand}
+
+  import CommunityDemoWeb.Community.Components.Lists.{
+    List001,
+    List002,
+    List003,
+    List004,
+    List005,
+  }
 
   def mount(_params, _session, socket) do
     socket =
