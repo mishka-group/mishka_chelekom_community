7adcc740e32218a355c77e36d297904ef3f84b03
Mona Aghili <28711325+MonaAghili@users.noreply.github.com>
2025-01-30 11:52:55 +0330

    add new component to navbar

--- community_demo/lib/community_demo_web/live/navbar_live.ex
+++ community_demo/lib/community_demo_web/live/navbar_live.ex
@@ -1,12 +1,12 @@
 defmodule CommunityDemoWeb.Demo.Community.NavbarLive do
   use CommunityDemoWeb, :live_view
-  import CommunityDemoWeb.CoreComponents, except: [button: 1]
-  import CommunityDemoWeb.Components.{CustomHeader, CustomCommand, Button}
+  import CommunityDemoWeb.Components.{CustomHeader, CustomCommand, SearchField}
 
   import CommunityDemoWeb.Community.Components.Navbars.{
     Navbar001,
     Navbar002,
     Navbar003,
+    Navbar004,
   }
 
   def mount(_params, _session, socket) do
