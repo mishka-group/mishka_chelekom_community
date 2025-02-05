49b18b50e839bdb97a6ae116935223706d2b5845
Shahryar Tavakkoli <shahryar.tbiz@gmail.com>
2025-02-05 21:13:17 +0330

    Add footer components json

---
+++ components/component_footer_001.exs
@@ -0,0 +1,12 @@
+[
+  component_footer_001: [
+    name: "component_footer_001",
+    args: [
+      only: ["community_footer_001"],
+      helpers: [],
+      module: ""
+    ],
+    optional: [],
+    necessary: ["footer", "button", "text_field"]
+  ]
+]
---
+++ components/component_footer_002.exs
@@ -0,0 +1,12 @@
+[
+  component_footer_002: [
+    name: "component_footer_002",
+    args: [
+      only: ["community_footer_002"],
+      helpers: [],
+      module: ""
+    ],
+    optional: [],
+    necessary: ["footer"]
+  ]
+]
